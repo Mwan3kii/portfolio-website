cca32e6fe85769482e223e44055950853bf5365e
Mwan3kii <agathawanja303@gmail.com>
2024-11-18 21:28:55 +0300

    Update cards by mapping through the items

--- js/script.js
+++ js/script.js
@@ -1,3 +1,54 @@
+const projectInfo = [
+    {
+        id: 1,
+        projectName: 'Todo App to Manage your Tasks',
+        projectInfo:
+            'A daily tracking of privately personalized activities; no accounts or sign-ups required.',
+        tech: ['HTML', 'CSS', 'Javascript'],
+        img: 'projects/center_back.png',
+    },
+    {
+        id: 2,
+        projectName: 'Data Dashboard Healthcare',
+        projectInfo:
+            "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+        tech: ['HTML', 'Bootstrap', 'Ruby'],
+        img: 'projects/center_back.png',
+    },
+    {
+        id: 3,
+        projectName: 'Website Portfolio',
+        projectInfo:
+            "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+        tech: ['HTML', 'Bootstrap', 'Ruby'],
+        img: 'projects/center_back.png',
+    },
+    {
+        id: 4,
+        projectName: 'Profesional Art Printing Data',
+        projectInfo:
+            "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+        tech: ['HTML', 'Bootstrap', 'Ruby'],
+        img: 'projects/center_back.png',
+    },
+    {
+        id: 5,
+        projectName: 'Data Dashboard Healthcare',
+        projectInfo:
+            "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+        tech: ['HTML', 'Bootstrap', 'Ruby'],
+        img: 'projects/center_back.png',
+    },
+    {
+        id: 6,
+        projectName: 'Website Portfolio',
+        projectInfo:
+            "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+        tech: ['HTML', 'Bootstrap', 'Ruby'],
+        img: 'projects/center_back.png',
+    },
+];
+
 function mobileMenu() {
     const hamburger = document.querySelector('.menu-bar');
     const mobileMenu = document.getElementById('menu-page');
@@ -17,27 +68,24 @@ function mobileMenu() {
 function showPopup() {
     const mainPopupDiv = document.createElement('div');
     mainPopupDiv.classList.add('popup');
-    const xIcon = document.createElement('img');
-    xIcon.classList.add('x-icon');
-    xIcon.setAttribute('src', '../assets/images/close.png')
-    mainPopupDiv.appendChild(xIcon);
     const secondaryPopupDiv = document.createElement('div');
     secondaryPopupDiv.classList.add('popup_content');
     mainPopupDiv.appendChild(secondaryPopupDiv);
     const imageDiv = document.createElement('div');
-    imageDiv.setAttribute('id','popup_img');
-    
+    imageDiv.setAttribute('id', 'popup_img');
+    const close_icon = document.createElement('img');
+    close_icon.setAttribute('class', 'close_button');
+    close_icon.setAttribute('src', '../assets/images/Icon.png');
+    close_icon.setAttribute('onclick', 'closePopup()');
+    const desktop_close_icon = document.createElement('img');
+    desktop_close_icon.setAttribute('class', 'desktop_close_button');
+    desktop_close_icon.setAttribute('src', '../assets/images/desktop_close.png');
+    desktop_close_icon.setAttribute('onclick', 'closePopup()');
     const img = document.createElement('img');
-    img.classList.add('popup-img')
     img.setAttribute('src', '../assets/images/Snapshot Portfolio.jpg');
-    img.setAttribute('width', '100%');
-    img.setAttribute('height', '220px');
-    img.setAttribute('border-radius', '5px');
-    const cancelIcon = document.createElement('img');
-    cancelIcon.classList.add('cancel-icon');
-    cancelIcon.setAttribute('src', '../assets/images/cancel.png')
-    img.setAttribute('position', 'relative');
-    img.appendChild(cancelIcon);
+    img.setAttribute('class', 'main_img');
+    imageDiv.appendChild(close_icon);
+    imageDiv.appendChild(desktop_close_icon);
     imageDiv.appendChild(img);
     const titleSection = document.createElement('div');
     titleSection.classList.add('title-popup');
@@ -113,16 +161,61 @@ function showPopup() {
     buttonDiv.appendChild(liveButton);
     buttonDiv.appendChild(sourceButton);
     secondaryPopupDiv.appendChild(buttonDiv);
-    
-
-    
-
     return mainPopupDiv;
 }
 
-window.addEventListener('load', ()=> {
-   const popupWindowSection = document.getElementById('popWindow');
-   if (popupWindowSection) {
+function showDynamicproject() {
+    const divDynamic = projectInfo.map((item) => {
+        const mainCard = document.createElement('div');
+        mainCard.classList.add('card');
+        const cardImage = document.createElement('div');
+        cardImage.classList.add('card-image')
+        const cardContent = document.createElement('div');
+        cardContent.classList.add('card-content');
+        mainCard.appendChild(cardImage);
+        mainCard.appendChild(cardContent);
+        const heading = document.createElement('h3');
+        heading.innerText = `${item.projectName}`;
+        const Tags = document.createElement('div');
+        Tags.classList.add('tags');
+        cardContent.appendChild(heading);
+        cardContent.appendChild(Tags);
+        const spanTag = document.createElement('span');
+        spanTag.classList.add('tag');
+        spanTag.innerText = 'JavaScript';
+        const popupButton = document.createElement('button');
+        popupButton.setAttribute('class', 'btn');
+        popupButton.setAttribute('type', 'button');
+        popupButton.setAttribute('onclick', 'openPopup()')
+        popupButton.innerText = 'See Project';
+        Tags.appendChild(spanTag);
+        cardContent.appendChild(popupButton);
+
+        return mainCard;
+    })
+
+    return divDynamic;
+}
+
+function openPopup() {
+    const popupWindow = document.querySelector('.popup');
+    popupWindow.style.display = 'block';
+}
+
+function closePopup() {
+    const popupWindow = document.querySelector('.popup');
+    popupWindow.style.display = 'none';
+}
+
+window.addEventListener('load', () => {
+    const popupWindowSection = document.getElementById('popWindow');
+    if (popupWindowSection) {
         popupWindowSection.appendChild(showPopup());
-   }
+    }
+
+    // document.querySelector('.card-section').innerHTML = showDynamicproject().join('');
+    const cardSection = document.querySelector('.card-section');
+    if (cardSection) {
+        showDynamicproject().forEach(card => cardSection.appendChild(card));
+    }
 })
