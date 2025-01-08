1f9cac98c77686a3af8039dc1d687c2f9f678f69
Mwan3kii <agathawanja303@gmail.com>
2025-01-08 21:07:15 +0300

    Fix mobile version styling

--- js/script.js
+++ js/script.js
@@ -47,11 +47,11 @@ function showPopup() {
     imageDiv.setAttribute('id', 'popup_img');
     const close_icon = document.createElement('img');
     close_icon.setAttribute('class', 'close_button');
-    close_icon.setAttribute('src', '../assets/images/Icon.png');
+    close_icon.setAttribute('src', 'assets/images/Icon.png');
     close_icon.setAttribute('onclick', 'closePopup()');
     const desktop_close_icon = document.createElement('img');
     desktop_close_icon.setAttribute('class', 'desktop_close_button');
-    desktop_close_icon.setAttribute('src', '/assets/images/desktop_close.png');
+    desktop_close_icon.setAttribute('src', '../assets/images/desktop_close.png');
     desktop_close_icon.setAttribute('onclick', 'closePopup()');
     const img = document.createElement('img');
     img.setAttribute('src', '../assets/images/Snapshot Portfolio.jpg');
